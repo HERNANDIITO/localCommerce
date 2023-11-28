b82b8569abcb810c51b9365a70a8c477bd717200
HERNANDIITO <pablohhgg@gmail.com>
2023-11-28 18:58:48 +0100

    contacto

--- localCommerce/src/app/views/public/support/support.component.ts
+++ localCommerce/src/app/views/public/support/support.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'lc-support',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./support.component.scss']
 })
 export class SupportComponent {
-
+send() {
+throw new Error('Method not implemented.');
+}
+  supportForm = new FormGroup({
+    name:  new FormControl(null, Validators.required),
+    email: new FormControl(null, Validators.required),
+    tlf:   new FormControl(null, Validators.required),
+    msg:   new FormControl(null, Validators.required)
+  });
 }
